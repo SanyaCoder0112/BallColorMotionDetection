9ba6340c1426e81e3950d7b1df6053a60dd85d2b
Sanya <sanyamishra4921@gmail.com>
2023-12-10 19:33:31 +0530

    getting center of images

--- BallDetection.py
+++ BallDetection.py
@@ -9,7 +9,14 @@ cap = cv2.VideoCapture(0)
 
 while True:
     _,frame =cap.read()
-    print("hiiii")
+    hsv_frame=cv2.cvtColor(frame,cv2.COLO_BGR2HSV)
+    height,width,_=frame.shape
+    cx=int(width/2)
+    cy=int(height/2)
+    #pick pixel value 
+    pixel_center =frame[cy,cx]
+    print(pixel center)
+    cv2.circle("frame",(cx,cy),5,(255,0,0),3)
     cv2.imshow("frame",frame )
     key=cv2.waitKey(1)
     if key ==27:
